c69e795a6462aced0dc0d0a1af9b8e0ead6624e0
MG <50220378+marianoguggiana@users.noreply.github.com>
2022-08-29 18:44:57 -0300

    Create index.js

---
+++ Entrega-01/index.js
@@ -0,0 +1,22 @@
+#CONSIGNA1
+// let num = parseInt(prompt("Ingresar Numero"));
+
+// for (let i = 1; i <= 5; i++) {
+//     let resultado = num + i ;
+//     alert(num +" + "+ i +" = "+ resultado);
+// }
+
+#CONSIGNA2
+// let texto = prompt("Ingresar texto:");
+
+// while(texto != "ESC" ){
+//     alert("El usuario ingresó "+ texto);
+//     texto = prompt("Ingresar otro texto:");
+// }
+
+#CONSIGNA3
+// let num = parseInt(prompt("Ingresar numero:"))
+
+// for (let i = 1; i <= num; i++){
+//     console.log("HOLA")
+// }
